ea7a7747e59acf56af9bf9b41d2d6eecbb5a6c40
diegooliveira150 <diego1504camargo@gmail.com>
2025-03-12 11:31:21 -0300

    Add files via upload

--- felicidade/script.js
+++ felicidade/script.js
@@ -1,21 +1,42 @@
-//Importando o mudlo HTTP
-const http = require('http');
+    const fs = require('fs');
+    const path = require('path')
+    const express = require('express')
 
-//Configurando as informações do servidor 
-const hostname = '127.0.0.1';
-const port = 3000;
+    const app = express();
 
-//Criando o servidor usando uma função com nome diferente 
-const meuSevidor = http.createServer((req,res) => {
-    //Configurando o cabeçalho da resposta 
-    res.statusCode = 200;
-    res.setHeader('Content Type','text/plain');
+    const port = 3000;
 
-    //Enviando uma mensagem como resposta 
-    res.end('Bem-vindo ao meu servidor Node.js!\n'); 
-});
+    const carrosPath = path.join(__dirname, 'carros_classicos.json');
 
-//Iniciando o servidor  escutando na porta especificada 
-meuSevidor.listen(port,hostname, () => {
-    console.log(`Servidor rodando em http://${hostname}:${port}/`);
-})
+    app.use(express.json());
+    app.use(express.urlencoded({extended: true}));
+
+    //Lendo os dados do arquivo json 
+    let carrosData  = fs.readFileSync(carrosPath,'utf-8');
+    let carros = JSON.parse(carrosData);
+
+    //Função para salvar os dados atualizados no arquivo JSON
+    function salvarDados() {
+        fs.writeFileSync(carrosPath, JSON.stringify(carros, null, 2));
+    }
+
+    app.get ('adicionar-carro', (req,res)  =>{
+        res.sendFile(pat.join(_dirname,'adicionar.html'))
+    });
+
+    app.post('/adicionar-carro', (req, res) => {
+        const novoCarro = req.body;
+
+        if (carros.find(carro => carro.nome.toLowerCase() === novoCarro.nome.toLowerCase())) {
+            res.send('<h1>Carro já existe. Não é possível adicionar duplicatas.</h1>');
+            return;
+        }
+
+        carros.push(novoCarro);
+        salvarDados();
+        res.send('<h1>Carro adicionado com sucesso!</h1>');
+    });
+
+    app.listen(port, () => {
+        console.log(`Servidor iniciado em http://localhost:${port}`);
+    });
